c81ef15944d3e985607da819395ef307086252c1
Nam Nguyen <namnd.hn@gmail.com>
2019-08-06 11:48:55 +0700

    Fix routes

--- src/ExpressAPI.php
+++ src/ExpressAPI.php
@@ -4,7 +4,12 @@
 namespace Isofman\LaravelExpressAPI;
 
 
+use Illuminate\Routing\Router;
+
 class ExpressAPI
 {
-
+    public static function routes()
+    {
+        Router::get('/express-api', 'Isofman\LaravelExpressAPI\DataResolveController@index');
+    }
 }
